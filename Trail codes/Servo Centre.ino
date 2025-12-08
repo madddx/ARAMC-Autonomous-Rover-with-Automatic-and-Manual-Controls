300757fbf07d881630c365b5a4f37cbe2158676d
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-12-08 10:15:11 +0530

    Update Servo Centre.ino

--- Trail codes/Servo Centre.ino
+++ Trail codes/Servo Centre.ino
@@ -1,11 +1,13 @@
 #include <Servo.h>
 
-Servo myServo;
+Servo myServo1;
+Servo myServo2;
 
 void setup() {
-  myServo.attach(9);   // Connect servo signal wire to pin 9
-  myServo.attach(8);   // Connect servo signal wire to pin 8
-  myServo.write(90);   // Center the servo at 90 degrees
+  myServo1.attach(9);   // Connect servo signal wire to pin 9
+  myServo2.attach(8);   // Connect servo signal wire to pin 8
+  myServo1.write(0);   // Center the servo at 90 degrees
+  myServo2.write(90);
 }
 
 void loop() {
